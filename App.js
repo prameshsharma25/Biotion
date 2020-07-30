77a93a504e090f2511d51ad847bd7958e9979fba
Pramesh Sharma <prameshsharma25@gmail.com>
2020-07-29 21:18:39 -0700

    changed font color

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={{color: "Blue", fontSize:18 }}>Welcome to Biotion!</Text>
+      <Text style={{color: "#334", fontSize:18 }}>Welcome to Biotion!</Text>
       <StatusBar style="auto" />
     </View>
   );
