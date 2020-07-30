40ee38ea8870a96e92ede4434e23770295f8f44f
Pramesh Sharma <prameshsharma25@gmail.com>
2020-07-30 11:28:45 -0700

    added loading screen

--- App.js
+++ App.js
@@ -1,15 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {createAppContainer, createSwitchNavigator} from 'react-navigation'
+
+import LoginScreen from './screens/LoginScreen'
+import LoadingScreen from './screens/LoadingScreen'
+import DashboardScreen from './screens/DashboardScreen'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={{color: "#334", fontSize:18 }}>Welcome to Biotion!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <AppNavigator/>
 }
+
+const AppSwitchNavigator = createSwitchNavigator({
+  LoadingScreen:LoadingScreen,
+  LoginScreen:LoginScreen,
+  DashboardScreen:DashboardScreen
+})
+
+const AppNavigator = createAppContainer(AppSwitchNavigator);
 
 const styles = StyleSheet.create({
   container: {
