84cc4695bbe798df5de89aee81d9bef70bc25fbe
Pramesh Sharma <prameshsharma25@gmail.com>
2020-07-29 20:48:04 -0700

    test react native styling

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Welcome to Biotion!</Text>
+      <Text style={{color: "Blue", fontSize:18 }}>Welcome to Biotion!</Text>
       <StatusBar style="auto" />
     </View>
   );
